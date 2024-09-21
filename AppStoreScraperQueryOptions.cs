5c128a1c545074c37af0b08188f6edf1bbf36a52
APIVerve Team <hello@apiverve.com>
2024-09-21 01:02:56 -0500

    Bug Fixes

---
+++ AppStoreScraperQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class AppStoreScraperQueryOptions {
+public string appid { get; set; }
+public string country { get; set; }
+}
+
+}
